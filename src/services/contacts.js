6516bba18bf6efc37ada98eab757059056167744
Андрей <a.lyashenko95@gmail.com>
2025-04-10 15:53:14 +0300

    not-ok

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,6 @@
 import { Contact } from "../db/models/contact.js";
 
-export const getContacts = async (req, res) => {
+export const getContacts = async () => {
   const contacts = await Contact.find();
   return contacts;
 };
